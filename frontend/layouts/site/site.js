13b5852c3fe3fd111cb1bd9bb07a8fedc29fc202
junki105 <junki.dev@gmail.com>
2018-02-14 14:15:58 +0900

    refact: debug site.js

--- frontend/layouts/site/site.js
+++ frontend/layouts/site/site.js
@@ -1,8 +1,8 @@
 import "./site.css";
 
 document.addEventListener("DOMContentLoaded", () => {
-  document.getElementById("selectedLocale").addEventListener("change", () => {
-    const selectedOption = this.options[this.selectedIndex];
+  document.getElementById("selectedLocale").addEventListener("change", ev => {
+    const selectedOption = ev.target.options[ev.target.selectedIndex];
     const url = selectedOption.getAttribute("data-url");
     if (url) {
       window.location.href = url;
